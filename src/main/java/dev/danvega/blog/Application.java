2f7044a32e8a3b54e7e7f8b3edd4921bfc94fab2
Dan Vega <danvega@gmail.com>
2022-11-03 15:56:02 -0400

    adding some test data for dev and prod

--- src/main/java/dev/danvega/blog/Application.java
+++ src/main/java/dev/danvega/blog/Application.java
@@ -1,7 +1,16 @@
 package dev.danvega.blog;
 
+import dev.danvega.blog.model.Author;
+import dev.danvega.blog.model.Comment;
+import dev.danvega.blog.model.Post;
+import dev.danvega.blog.repository.AuthorRepository;
+import dev.danvega.blog.repository.PostRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +19,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Profile({"dev","prod"})
+	@Bean
+	CommandLineRunner run(PostRepository postRepository, AuthorRepository authorRepository) {
+		return args -> {
+			AggregateReference<Author,Integer> author = AggregateReference.to(authorRepository.save(new Author(null, "Dan", "Vega", "danvega@gmail.com", "dvega")).id());
+
+			Post post = new Post( "Dan's First Post", "This is Dan's First Post",author);
+			post.addComment(new Comment( "Dan", "This is a comment"));
+			post.addComment(new Comment( "John", "This is another comment"));
+			postRepository.save(post);
+		};
+	}
 }
